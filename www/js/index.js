bcaddc5db3a82e7ab52e640ccb2dfb37a5267911
Benjamin Pick <b.pick@yellowtree.de>
2013-10-30 17:14:05 +0100

    first commit

---
+++ www/js/index.js
@@ -0,0 +1,77 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+var app = {
+    // Application Constructor
+    initialize: function() {
+        this.bindEvents();
+    },
+    // Bind Event Listeners
+    //
+    // Bind any events that are required on startup. Common events are:
+    // 'load', 'deviceready', 'offline', and 'online'.
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
+    // deviceready Event Handler
+    //
+    // The scope of 'this' is the event. In order to call the 'receivedEvent'
+    // function, we must explicity call 'app.receivedEvent(...);'
+    onDeviceReady: function() {
+        $(document).on('mobileinit', function() {
+            // Allow Cross-Domain Requests!
+            $.support.cors = true; 
+            $.mobile.allowCrossDomainPages = true;
+            
+            // Für installierte Apps
+            $.mobile.pushStateEnabled = false;
+        });
+
+        $('#take_photo').on('click', app.camera.getPicture);
+        app.receivedEvent('deviceready');
+        
+    },
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {
+        var parentElement = document.getElementById(id);
+        var listeningElement = parentElement.querySelector('.listening');
+        var receivedElement = parentElement.querySelector('.received');
+
+        listeningElement.setAttribute('style', 'display:none;');
+        receivedElement.setAttribute('style', 'display:block;');
+
+        console.log('Received Event: ' + id);
+    },
+    
+    camera: {
+        getPicture: function() {
+            alert('ok');
+            navigator.camera.getPicture(app.camera.onSuccess, app.camera.onError);
+        },
+        onError: function(message) {
+            alert('Failed: ' + message);
+        },
+        onSuccess: function(imageData) {
+            $('#img').attr('src') = 'data:image/jpeg;base64,' + imageData;
+        }
+    }
+};
+//app.onDeviceReady();
+
+
+app.bindEvents();
